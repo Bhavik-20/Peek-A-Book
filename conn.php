448c782c26426c77af70a47bc520abb064e7e68a
Bhavik-20 <59500727+Bhavik-20@users.noreply.github.com>
2022-07-08 11:36:00 +0530

    migated to new db

--- conn.php
+++ conn.php
@@ -1,8 +1,13 @@
 <?php
 
-$HOST="g1ulxzso4stk.us-east-1.psdb.cloud";
-$USERNAME="7ky85vsj9r7e";
-$PASSWORD="pscale_pw_MjVZvRR2vGs9XwZNXzvhwoUOgvKEJpB-4hDrwxcauW8";
-$DATABASE="peek-a-book";
+// $HOST="g1ulxzso4stk.us-east-1.psdb.cloud";
+// $USERNAME="7ky85vsj9r7e";
+// $PASSWORD="pscale_pw_MjVZvRR2vGs9XwZNXzvhwoUOgvKEJpB-4hDrwxcauW8";
+// $DATABASE="peek-a-book";
+
+$HOST="sql6.freemysqlhosting.net";
+$USERNAME="sql6504812";
+$PASSWORD="5ted3QCfme";
+$DATABASE="sql6504812";
 
 ?>

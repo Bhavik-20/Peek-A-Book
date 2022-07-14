5a93b0b2351533f732aa2b1caec73ee357a0b977
Bhavik-20 <59500727+Bhavik-20@users.noreply.github.com>
2022-07-14 10:41:53 +0530

    Composer: added gd lib

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '81072c572c6eb88f9ca51dcd0e9e325aa89d449b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '81072c572c6eb88f9ca51dcd0e9e325aa89d449b',
+            'dev_requirement' => false,
+        ),
+    ),
+);

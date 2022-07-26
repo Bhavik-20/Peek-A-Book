611670ecd0fc0fd869c8b4d03985441fea293a5f
Bhavik-20 <59500727+Bhavik-20@users.noreply.github.com>
2022-07-26 11:27:00 +0530

    New db connection

--- conn.php
+++ conn.php
@@ -1,8 +1,20 @@
 <?php
+// x-x-x-x-x-x-x-x-x-x-x-x-x-x-x- EXPIRED x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-
+// $HOST="sql6.freemysqlhosting.net";
+// $USERNAME="sql6504812";
+// $PASSWORD="5ted3QCfme";
+// $DATABASE="sql6504812";
 
-$HOST="sql6.freemysqlhosting.net";
-$USERNAME="sql6504812";
-$PASSWORD="5ted3QCfme";
-$DATABASE="sql6504812";
+$HOST="remotemysql.com";
+$USERNAME="2b5eFPIIqL";
+$PASSWORD="Jxk9YCwADj";
+$DATABASE="2b5eFPIIqL";
+
+// Username: 2b5eFPIIqL
+// Database name: 2b5eFPIIqL
+// Password: Jxk9YCwADj
+// Server: remotemysql.com
+// Port: 3306
+
 
 ?>

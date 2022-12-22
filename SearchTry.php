f5a6540a59b4d9b7b3d789be5391ad80dec01e2e
Muskaan-Nandu <nandumuskaan@gmail.com>
2022-12-22 09:57:51 -0500

    try

--- SearchTry.php
+++ SearchTry.php
@@ -35,9 +35,6 @@ echo "</select>";
 mysqli_close($conn);
 }
 ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" /> 
 
 <script type="text/javascript">
 $("#searchddl").chosen();

808ab39da6d58091962f66ed251ed6a1e7d191b0
Bhavik-20 <59500727+Bhavik-20@users.noreply.github.com>
2022-12-22 08:48:24 -0600

    Try

--- SearchTry.php
+++ SearchTry.php
@@ -37,7 +37,7 @@ mysqli_close($conn);
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js"></script>
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" />  -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" /> 
 
 <script type="text/javascript">
 $("#searchddl").chosen();
